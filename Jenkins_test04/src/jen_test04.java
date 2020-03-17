48e4d357313b2164fdabad80917a83d72d3de490
Sharan <43085942+sharan-gowda@users.noreply.github.com>
2020-03-17 17:52:03 +0530

    Add files via upload

---
+++ Jenkins_test04/src/jen_test04.java
@@ -0,0 +1,13 @@
+
+public class jen_test04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<4;i++)
+		{
+			System.out.println("EXECUTING JENTEST04");
+		}
+
+	}
+
+}
