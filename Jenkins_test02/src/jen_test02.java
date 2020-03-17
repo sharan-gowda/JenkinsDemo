3343245157a8b06e5c394d00a84639c5d12a06d4
Sharan <43085942+sharan-gowda@users.noreply.github.com>
2020-03-17 17:50:25 +0530

    Add files via upload

---
+++ Jenkins_test02/src/jen_test02.java
@@ -0,0 +1,13 @@
+
+public class jen_test02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<2;i++)
+		{
+			System.out.println("EXECUTING JENTEST02");
+		}
+
+	}
+
+}
